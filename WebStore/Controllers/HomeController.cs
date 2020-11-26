33816dfbbefa3356912b84e4fc80d5829f3dbccc
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-11-26 21:25:53 +0300

    Created Views, Models

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -11,18 +12,31 @@ namespace WebStore.Controllers
     {
         private IConfiguration _configuration;
 
+        private static readonly List<Employee> Employees = new List<Employee>
+        {
+            new Employee() {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 32},
+            new Employee() {Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 22},
+            new Employee() {Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 42},
+        };
+
         public HomeController( IConfiguration configuration)
         {
             _configuration = configuration;
         }
         public IActionResult Index()
         {
-            return Content(_configuration["ControllerActionText"]);
+            return View();
+            //return Content(_configuration["ControllerActionText"]);
         }
 
         public IActionResult SecondAction()
         {
             return Content("Second controller action");
+        }
+
+        public IActionResult Employes()
+        {
+            return View(Employees);
         }
     }
 }
