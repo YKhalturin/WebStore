5d6a70e27a13c543643961881b6c276d0b24de59
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-03 16:13:25 +0300

    added employee interface

---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+        Employee Set(int id);
+
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(Employee employee);
+        void SaveChanges();
+    }
+}
