615603be17cb3a6e0f1fe4566881aa94dd068359
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-05 11:49:52 +0300

    added validation cases

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Models
 {
@@ -10,7 +12,7 @@ namespace WebStore.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Patronymic { get; set; }
+        public string Patronymic { get; set; } 
         public int Age { get; set; }
         public DateTime BirthDay { get; set; }
 
