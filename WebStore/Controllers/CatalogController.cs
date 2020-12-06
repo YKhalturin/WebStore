d018af98adfcf0326434fba11cc66303dc3eb9f4
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-06 19:39:16 +0300

    Finished with brands and catalogs

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Shop(int? BrandId, int? SectionId)
+        {
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products
+                    .OrderBy(p => p.Order)
+                    .Select(p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl
+                    })
+            });
+        }
+
+        //public IActionResult Shop2(ProductFilter filter)
+        //{
+        //        var products = _ProductData.GetProducts(filter);
+
+        //        return View("Shop", new CatalogViewModel
+        //        {
+        //            SectionId = filter.SectionId,
+        //            BrandId = filter.BrandId,
+        //            Products = products
+        //                .OrderBy(p => p.Order)
+        //                .Select(p => new ProductViewModel
+        //                {
+        //                    Id = p.Id,
+        //                    Name = p.Name,
+        //                    Price = p.Price,
+        //                    ImageUrl = p.ImageUrl
+        //                })
+        //        });
+        //}
+
+    }
+    
+}
