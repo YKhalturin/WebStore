27f81ef70450743ddff7444fb6dcb848d8e63a40
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-22 17:02:59 +0300

    Added login/logout for admin

--- WebStore/Areas/Admin/Controllers/UsersController.cs
+++ WebStore/Areas/Admin/Controllers/UsersController.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class UsersController : Controller
     {
         public IActionResult Index()
