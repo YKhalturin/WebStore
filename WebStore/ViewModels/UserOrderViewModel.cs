2fe5f509d4f0ab56253c186a89a3ac123ea171e5
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-24 17:22:26 +0300

    Small changes

--- WebStore/ViewModels/UserOrderViewModel.cs
+++ WebStore/ViewModels/UserOrderViewModel.cs
@@ -2,19 +2,19 @@
 {
     public class UserOrderViewModel
     {
-        public readonly int _orderId;
-        public readonly string _orderName;
-        public readonly string _orderPhone;
-        public readonly string _orderAddress;
-        public readonly decimal _sum;
+        public readonly int OrderId;
+        public readonly string OrderName;
+        public readonly string OrderPhone;
+        public readonly string OrderAddress;
+        public readonly decimal Sum;
 
         public UserOrderViewModel(in int orderId, string orderName, string orderPhone, string orderAddress, decimal sum)
         {
-            _orderId = orderId;
-            _orderName = orderName;
-            _orderPhone = orderPhone;
-            _orderAddress = orderAddress;
-            _sum = sum;
+            OrderId = orderId;
+            OrderName = orderName;
+            OrderPhone = orderPhone;
+            OrderAddress = orderAddress;
+            Sum = sum;
         }
     }
 }
