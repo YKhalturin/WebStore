dee11bc78fed334a8c9e6e3cccc72fbd12477008
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2020-12-24 17:21:47 +0300

    Created user profile

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrders(User.Identity!.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel (
+                order.Id,
+                order.Name,
+                order.Phone,
+                order.Address,
+                order.Items.Sum(item => item.Price * item.Quantity)
+            )));
+        }
+    }
+}
---
+++ WebStore/ViewModels/UserOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WebStore.ViewModels
+{
+    public class UserOrderViewModel
+    {
+        public readonly int _orderId;
+        public readonly string _orderName;
+        public readonly string _orderPhone;
+        public readonly string _orderAddress;
+        public readonly decimal _sum;
+
+        public UserOrderViewModel(in int orderId, string orderName, string orderPhone, string orderAddress, decimal sum)
+        {
+            _orderId = orderId;
+            _orderName = orderName;
+            _orderPhone = orderPhone;
+            _orderAddress = orderAddress;
+            _sum = sum;
+        }
+    }
+}
